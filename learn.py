c9bd3db0c0524aaa84ca5cd5a94c926344d59dd0
Kevin He <49215277+kevinhe5@users.noreply.github.com>
2020-11-19 19:47:35 -0500

    Add files via upload

--- learn.py
+++ learn.py
@@ -18,9 +18,39 @@ while x != y:
 
 print("exited")
 """
-list = [3, 6, 7, 6, 7, 10, 12]
+list = [3, 6, 7, 6, 7, 10, 12, 13, 11, 12]
+str_list = ["Kevin", "Coding", "Club", "CB South", "Python"]
+list2 = [1, 2, 3, 4]
 
-def my_max(list):
+def second_largest(list):
+    list.remove(max(list))
+    return max(list)
+
+print(second_largest(list))
+
+#max()
+#min()
+
+str_list.sort()
+print(str_list)
+str_list.remove("Club")
+print(str_list)
+
+#append() #adds element to end of list
+#clear() #removes all elements from list
+#copy() #returns a copy of list
+#count() #returns number of elements with value
+#extend() #for combining elements in two lists
+#index() #returns index of first element with value
+#insert() #inserts element at index position
+#pop() #removes the element index position
+#remove() #removes first item with value
+#sort() sorts list
+#reverse()
+
+#in, not in
+
+"""def my_max(list):
     temp_max = list[0]
     for num in list:
         if num > temp_max:
@@ -36,10 +66,10 @@ def my_min(list):
 
     return temp_max
 
-print(my_min(list))
+print(my_min(list))"""
 
-
-
+x = input("What's your name?")
+print(x)
 
 
 
